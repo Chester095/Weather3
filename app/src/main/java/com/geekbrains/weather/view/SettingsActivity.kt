14edff1893bcf4b89578dfc6f8fdb8c19cf10bd6
Chester095 <Chester095@users.noreply.github.com>
2022-01-28 16:08:54 +0300

    На кукую то жуткую тему срабаытвает переключатель. Да и фиг с ним. Не в этом суть.

--- app/src/main/java/com/geekbrains/weather/view/SettingsActivity.kt
+++ app/src/main/java/com/geekbrains/weather/view/SettingsActivity.kt
@@ -3,16 +3,36 @@ package com.geekbrains.weather.view
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.geekbrains.weather.R
 
 class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("NotifyDataSetChanged")
+    @SuppressLint("NotifyDataSetChanged", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val actionBar = supportActionBar
         actionBar!!.title = "Настройки"
         Log.d("!!! SettingsActivity", " onCreate")
         setContentView(R.layout.activity_settings)
+
+        // Declare the switch from the layout file
+        val btn = findViewById<SwitchCompat>(R.id.switch_button_dark_theme)
+
+        // set the switch to listen on checked change
+        btn.setOnCheckedChangeListener { _, isChecked ->
+            // if the button is checked, i.e., towards the right or enabled
+            // enable dark mode, change the text to disable dark mode
+            // else keep the switch text to enable dark mode
+            if (btn.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                btn.text = "Disable dark mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                btn.text = "Enable dark mode"
+            }
+        }
     }
 }
