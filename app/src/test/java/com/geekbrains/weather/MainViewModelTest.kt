aa773c1244d61c4c2fdbf91b730160a346df86d6
TimofeevS <chester_095@mail.ru>
2023-10-06 16:44:30 +0300

    В процессе создания мокито тестов

---
+++ app/src/test/java/com/geekbrains/weather/MainViewModelTest.kt
@@ -0,0 +1,16 @@
+package com.geekbrains.weather
+
+import com.geekbrains.weather.model.Repository
+import com.geekbrains.weather.viewmodel.MainViewModel
+import org.mockito.Mock
+
+class MainViewModelTest {
+
+    private lateinit var mainViewModel: MainViewModel
+
+    @Mock
+    private lateinit var repository: Repository
+
+/*    @Mock
+    private lateinit var*/
+}
